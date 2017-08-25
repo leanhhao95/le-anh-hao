a964b6b184b725d8a29cf3f4de21d4df276935c6
DatCrown <datcrown2110@gmail.com>
2017-08-25 14:36:50 +0700

    Food Tracker used core data

---
+++ FoodTracker/Controller/MealTableViewController.swift
@@ -0,0 +1,197 @@
+//
+//  MealTableViewController.swift
+//  FoodTracker
+//
+//  Created by Quốc Đạt on 25.08.17.
+//  Copyright © 2017 QuocDat. All rights reserved.
+//
+
+import UIKit
+import os.log
+import CoreData
+
+class MealTableViewController: UITableViewController {
+    
+    //MARK: Properties
+    
+    var meals = [Meal]()
+    fileprivate lazy var fetchedResultController: NSFetchedResultsController<Meal> = {
+        // Creat fetch request
+        let fetchRequest: NSFetchRequest<Meal> = Meal.fetchRequest()
+        
+        //Configure fetch request
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "rating", ascending: false)]
+        
+        //Creat fetch result controller
+        
+        let fetchResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: AppDelegate.shared.persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        // configure fetched result controller
+        fetchResultsController.delegate = self
+        return fetchResultsController
+        
+    } ()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        
+        // Load the sample data.
+        do {
+            try self.fetchedResultController.performFetch()
+        } catch {
+            let fetchError = error as NSError
+            print("Unable to Perform Request")
+            print("\(fetchError), \(fetchError.localizedDescription)")
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let meals = fetchedResultController.fetchedObjects else {
+            return 0
+        }
+        return meals.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "MealTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        }
+        
+        configureCell(cell, indexPath: indexPath)
+        
+        return cell
+    }
+    
+    func configureCell(_ cell: MealTableViewCell, indexPath: IndexPath) {
+        // Fetch Meal
+        
+        let meal = fetchedResultController.object(at: indexPath)
+        
+        // Configure Cell
+        cell.nameLabel.text = meal.name
+        cell.ratingControl.rating = Int(meal.rating)
+        cell.photoImageView.image = meal.photo as? UIImage
+    }
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Fetch Meal
+            let meal = fetchedResultController.object(at: indexPath)
+            // Delete Meal
+            meal.managedObjectContext?.delete(meal)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    //MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        super.prepare(for: segue, sender: sender)
+        
+        switch(segue.identifier ?? "") {
+            
+        case "AddItem":
+            os_log("Adding a new meal.", log: OSLog.default, type: .debug)
+            
+        case "ShowDetail":
+            guard let mealDetailViewController = segue.destination as? MealViewController else {
+                fatalError("Unexpected destination: \(segue.destination)")
+            }
+            
+            guard let selectedMealCell = sender as? MealTableViewCell else {
+                fatalError("Unexpected sender: \(sender)")
+            }
+            
+            guard let indexPath = tableView.indexPath(for: selectedMealCell) else {
+                fatalError("The selected cell is not being displayed by the table")
+            }
+            
+            mealDetailViewController.meal = fetchedResultController.object(at: indexPath)
+            
+        default:
+            fatalError("Unexpected Segue Identifier; \(segue.identifier)")
+        }
+    }
+    
+    
+    //MARK: Actions
+    
+    @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
+        tableView.reloadData()
+    }
+    
+}
+
+//MARK: -NSFetchedResultsControllerDelegate
+extension MealTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch  type {
+        case .insert:
+            if let newIndexPath = newIndexPath {
+                tableView.insertRows(at: [newIndexPath], with: .fade)
+            }
+            break
+        case .delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        case .update:
+            if let  indexPath = indexPath, let cell = tableView.cellForRow(at: indexPath) as? MealTableViewCell {
+                configureCell(cell, indexPath: indexPath)
+            }
+            break
+        default:
+            break
+        }
+        do {
+            try AppDelegate.shared.persistentContainer.viewContext.save()
+        } catch {
+            print("\(error), \(error.localizedDescription)")
+        }
+    }
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+}
+
+
---
+++ FoodTracker/View/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Quốc Đạt on 25.08.17.
+//  Copyright © 2017 QuocDat. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
